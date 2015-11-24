db58337cfc08c157c17aff38b0672343398bb0cf
Martijn Spruijt <m.spruijt@solin.nl>
2015-11-24 11:29:39 +0100

    New version of the app

--- core/components/login/main.js
+++ core/components/login/main.js
@@ -51,10 +51,7 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl',
-		onEnter: function($state) {
-			$state.go('mm_login.credentials', {siteurl: 'http://ricoh29.solin.nl'});
-		}
+        controller: 'mmLoginSiteCtrl'
     })
 
     .state('mm_login.credentials', {
